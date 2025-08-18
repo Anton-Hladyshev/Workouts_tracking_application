44d746ca6d9dc16793424926aec1e643f33d4df3
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2025-08-18 14:06:46 +0200

    add Client router

--- main.py
+++ main.py
@@ -8,22 +8,13 @@ from schemas.schemas import SubscriptionDTO, TrainingOnInputDTO, TrainingAddDTO,
 from schemas.exceptions import InvalidPermissionsError, TimeValidationError, BusinessRulesValidationError
 from db.database import ClientService, CoachService, async_session_factory
 from dotenv import load_dotenv
-import os
 from app.routers.auth import get_current_user, router as auth_router
+from app.routers.client import router as client_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
-
-def get_current_client(user: UserDTO = Depends(get_current_user)) -> UserDTO:
-    if user.role != Role.STUDENT:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Not enough permissions. You are not a coach.",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    
-    return user
+app.include_router(client_router)
 
 def get_curent_coach(user: UserDTO = Depends(get_current_user)) -> UserDTO:
     if user.role != Role.COACH:
@@ -152,74 +143,6 @@ async def update_training(
             "content": updated_training
         }
     }
-
-@app.get("/users/me/client", response_model=UserDTO)
-async def read_current_client(
-    current_user: Annotated[UserDTO, Depends(get_current_client)]
-) -> UserDTO:
-    service = ClientService(current_user)
-    return service.get_user()
-
-@app.get("/users/me/client/subscriptions/", response_model=List[TrainingDTO])
-async def read_own_subscriptions(
-    current_user: Annotated[UserDTO, Depends(get_current_client)]
-) -> List[TrainingDTO]:
-    service = ClientService(current_user)
-    subs = await service.show_my_trainings()
-    if len(subs) == 0:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="You have no subscriptions",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    return subs
-
-@app.get("/users/me/client/available_trainings/", response_model=List[TrainingDTO])
-async def read_own_available_trainings(
-    current_user: Annotated[UserDTO, Depends(get_current_client)] ) -> List[TrainingDTO]:
-    service = ClientService(current_user)
-    available_trainings = await service.show_available_trainings()
-    if len(available_trainings) == 0:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="You have no available trainings",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    return available_trainings
-
-@app.post("/users/me/client/subscribe/", response_model=SubscriptionDTO)
-async def subscribe_to_trainig(
-    training_id: int,
-    current_user: Annotated[UserDTO, Depends(get_current_client)]
-    ):
-    service = ClientService(current_user)
-    try:
-        new_subscription = await service.subscribe_to_training(
-            training_id=training_id
-        )
-
-        return new_subscription
-    except ValueError:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No available trainings with this ID",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    
-@app.delete("/users/me/client/unsubscribe", status_code=status.HTTP_204_NO_CONTENT)
-async def unsubscribe_from_training(
-    training_id: int,
-    current_user: Annotated[UserDTO, Depends(get_current_client)]
-):
-    service = ClientService(current_user)
-    try:
-        await service.unsubscribe_from_training(training_id=training_id)
-    except ValueError as e:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"A subscription on a training with id={training_id} was not found in your subscriptions."
-        )
-
 
 """Exceptions"""
 @app.exception_handler(TimeValidationError)
